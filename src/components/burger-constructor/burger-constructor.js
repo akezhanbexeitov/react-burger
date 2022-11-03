370da78561528418663847c1977f820706d84619
Akezhan <akidulatini@gmail.com>
2022-11-03 23:46:36 +0800

    Create OrderDetails and add hardcoded data inside

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -3,9 +3,19 @@ import IngredientConstructor from './ingredient-constructor'
 import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 import propTypes from 'prop-types'
 import ingredientType from '../../utils/types'
+import { useState } from 'react'
+import Modal from '../modal/Modal'
+import OrderDetails from '../order-details/order-details'
 
 const BurgerConstructor = (props) => {
     const { data } = props
+    const [isOpen, setIsOpen] = useState(false)
+
+    const modal = (
+        <Modal setIsOpen={setIsOpen}>
+            <OrderDetails />
+        </Modal>
+    )
 
     return (
         <>
@@ -15,10 +25,13 @@ const BurgerConstructor = (props) => {
             <div className={`${burgerConstructorStyles.price} pl-4 pr-4`}>
                 <p className="text text_type_digits-medium mr-2">610</p>
                 <CurrencyIcon type="primary" />
-                <Button className='button button_type_primary button_size_medium ml-10' type="primary" size="medium" htmlType='submit'>
-                    Оформить заказ
-                </Button>
+                <div onClick={() => setIsOpen(true)}>
+                    <Button className='button button_type_primary button_size_medium ml-10' type="primary" size="medium" htmlType='submit'>
+                        Оформить заказ
+                    </Button>
+                </div>
             </div>
+            {isOpen && modal}
         </>
     )
 }
