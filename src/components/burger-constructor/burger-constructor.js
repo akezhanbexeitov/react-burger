1c46b5b1573f2aa5d4e3f197e7c53cfa6f0884aa
Akezhan <akidulatini@gmail.com>
2022-11-11 14:55:42 +0800

    Calculate total price

--- src/components/burger-constructor/burger-constructor.js
+++ src/components/burger-constructor/burger-constructor.js
@@ -3,15 +3,18 @@ import IngredientConstructor from './ingredient-constructor'
 import { CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 // import propTypes from 'prop-types'
 // import ingredientType from '../../utils/types'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Modal from '../modal/modal'
 import OrderDetails from '../order-details/order-details'
 import withOverlay from '../modal-overlay/with-overlay'
+import IngredientContext from '../contexts/ingredient-context'
 
 const WithOverlayModal = withOverlay(Modal)
 
 const BurgerConstructor = () => {
     const [isOpen, setIsOpen] = useState(false)
+    const { ingredientConstructorState } = useContext(IngredientContext)
+    const { bun, ingredients } = ingredientConstructorState
 
     const modal = (
         <WithOverlayModal setIsOpen={setIsOpen}>
@@ -19,13 +22,26 @@ const BurgerConstructor = () => {
         </WithOverlayModal>
     )
 
+    const calculateTotalPrice = (bun, ingredients) => {
+        let total = 0
+        const bunsPrice = bun.price * 2
+        ingredients.map(item => total += item.price)
+        total += bunsPrice
+        return total
+    }
+
     return (
         <>
             <div className={`${burgerConstructorStyles.container} pt-25 pb-10`}>
                 <IngredientConstructor />
             </div>
             <div className={`${burgerConstructorStyles.price} pl-4 pr-4`}>
-                <p className="text text_type_digits-medium mr-2">610</p>
+                <p className="text text_type_digits-medium mr-2">{
+                        (Object.keys(bun).length > 0 || ingredients.length > 0) 
+                        ? calculateTotalPrice(bun, ingredients) 
+                        : 0
+                    }
+                </p>
                 <CurrencyIcon type="primary" />
                 <div onClick={() => setIsOpen(true)}>
                     <Button className='button button_type_primary button_size_medium ml-10' type="primary" size="medium" htmlType='submit'>
