4d4819768c99356bbd40279452ea1220dfdb3329
Akezhan <akidulatini@gmail.com>
2022-11-19 17:33:32 +0800

    Create ingredient-details reducer

--- src/components/burger-ingredients/ingredient-list.js
+++ src/components/burger-ingredients/ingredient-list.js
@@ -9,18 +9,18 @@ import withOverlay from '../modal-overlay/with-overlay'
 import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux'
 import { ADD_INGREDIENT_TO_CONSTRUCTOR } from '../../services/actions/ingredients-constructor'
+import { ADD_INGREDIENT_DETAILS } from '../../services/actions/ingredient-details'
 
 const WithOverlayModal = withOverlay(Modal)
 
 const IngredientList = forwardRef((props, ref) => {
     const { ingredientType, title } = props
     const [isOpen, setIsOpen] = useState(false)
-    const [ingredient, setIngredient] = useState(null)
     const dispatch = useDispatch()
 
     const modal = (
         <WithOverlayModal header="Детали ингредиента" setIsOpen={setIsOpen}>
-            {ingredient && <IngredientDetails ingredient={ingredient}/>}
+            <IngredientDetails />
         </WithOverlayModal>
     )
     
@@ -32,7 +32,6 @@ const IngredientList = forwardRef((props, ref) => {
                     return (
                         <li className={burgerIngredientsStyles.listItem} key={item['_id']} onClick={() => {
                             setIsOpen(true)
-                            setIngredient({...item})
                             dispatch({
                                 type: ADD_INGREDIENT_TO_CONSTRUCTOR,
                                 payload: {
@@ -42,6 +41,17 @@ const IngredientList = forwardRef((props, ref) => {
                                     type: item.type,
                                     id: item['_id'],
                                     key: uuidv4()
+                                }
+                            })
+                            dispatch({
+                                type: ADD_INGREDIENT_DETAILS,
+                                payload: {
+                                    name: item.name,
+                                    image_large: item.image_large,
+                                    calories: item.calories,
+                                    proteins: item.proteins,
+                                    fat: item.fat,
+                                    carbohydrates: item.carbohydrates
                                 }
                             })
                         }}>
--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import { ingredientsReducer } from './ingredients-list';
 import { ingredientsConstructorReducer } from './ingredients-constructor';
+import ingredientDetailsReducer from './ingredient-details';
 
 const rootReducer = combineReducers({
   ingredientsList: ingredientsReducer,
-  ingredientsConstructor: ingredientsConstructorReducer
+  ingredientsConstructor: ingredientsConstructorReducer,
+  ingredientDetails: ingredientDetailsReducer
 })
 
 export default rootReducer
