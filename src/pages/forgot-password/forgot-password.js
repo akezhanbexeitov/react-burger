aa0f85df31d5dc8d2e732f6fa596efed6eed558b
Akezhan <akidulatini@gmail.com>
2022-12-05 19:26:29 +0800

    Complete forgot/reset password logic

--- src/pages/forgot-password/forgot-password.js
+++ src/pages/forgot-password/forgot-password.js
@@ -2,10 +2,32 @@ import { useState } from "react"
 import { Link } from 'react-router-dom'
 import forgotPasswordStyles from './forgot-password.module.css'
 import { EmailInput, Button } from '@ya.praktikum/react-developer-burger-ui-components'
+import { BASE_URL } from "../../constants/constants"
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('')
-    
+
+    const onClick = (email) => {
+        const url = `${BASE_URL}/password-reset`
+        const body = { email: email }
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }
+        fetch(url, requestOptions)
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                return Promise.reject(`Ошибка ${res.status}`)
+            })
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
+
     return (
         <div className={forgotPasswordStyles.wrapper}>
             <div className={forgotPasswordStyles.container}>
@@ -18,7 +40,13 @@ const ForgotPassword = () => {
                     extraClass='mb-6'
                     placeholder="Укажите e-mail"
                 />
-                <Button htmlType="button" type="primary" size="medium" extraClass='mb-20'> {/* TODO: fix this button */}
+                <Button 
+                    htmlType="button" 
+                    type="primary" 
+                    size="medium" 
+                    extraClass='mb-20'
+                    onClick={() => onClick(email)}
+                > {/* TODO: fix this button */}
                     Восстановить
                 </Button>
                 <div className={`${forgotPasswordStyles.paragraph} mb-4`}>
--- src/pages/reset-password/reset-password.js
+++ src/pages/reset-password/reset-password.js
@@ -2,10 +2,35 @@ import resetPasswordStyles from './reset-password.module.css'
 import { PasswordInput, Button, Input } from '@ya.praktikum/react-developer-burger-ui-components'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import { BASE_URL } from "../../constants/constants"
 
 const ResetPassword = () => {
     const [password, setPassword] = useState('')
-    const [code, setCode] = useState(null)
+    const [token, setToken] = useState('')
+
+    const onClick = (password, token) => {
+        const url = `${BASE_URL}/password-reset/reset`
+        const body = { 
+            password: password,
+            token: token
+        }
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        }
+        fetch(url, requestOptions)
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                }
+                return Promise.reject(`Ошибка ${res.status}`)
+            })
+            .then(data => console.log(data))
+            .catch(error => console.log(error))
+    }
 
     return (
         <div className={resetPasswordStyles.wrapper}>
@@ -20,11 +45,17 @@ const ResetPassword = () => {
                 <Input 
                     type={'text'}
                     placeholder={'Введите код из письма'}
-                    onChange={e => setCode(e.target.value)}
-                    value={code}
+                    onChange={e => setToken(e.target.value)}
+                    value={token}
                     extraClass="mb-6"
                 />
-                <Button htmlType="button" type="primary" size="medium" extraClass='mb-20'> {/* TODO: fix this button */}
+                <Button 
+                    htmlType="button" 
+                    type="primary"
+                    size="medium" 
+                    extraClass='mb-20'
+                    onClick={() => onClick(password, token)}
+                > {/* TODO: fix this button */}
                     Сохранить
                 </Button>
                 <div className={`${resetPasswordStyles.paragraph} mb-4`}>
