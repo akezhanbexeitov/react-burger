fd9cb78906a97139b6cc865d91500d22d8041638
Akezhan <akidulatini@gmail.com>
2022-12-08 21:56:13 +0800

    Automatically redirect user from forgot-password to reset

--- src/pages/forgot-password/forgot-password.js
+++ src/pages/forgot-password/forgot-password.js
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom'
 import forgotPasswordStyles from './forgot-password.module.css'
 import { EmailInput, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 import { BASE_URL } from "../../constants/constants"
+import { useHistory } from 'react-router-dom'
+import LoadingSpinner from "../../components/loading-spinner/loading-spinner"
 
 const ForgotPassword = () => {
     const [email, setEmail] = useState('')
+    const history = useHistory()
+    const [isLoading, setIsLoading] = useState(false)
 
     const onClick = (email) => {
+        setIsLoading(true)
         const url = `${BASE_URL}/password-reset`
         const body = { email: email }
         const requestOptions = {
@@ -20,40 +25,46 @@ const ForgotPassword = () => {
         fetch(url, requestOptions)
             .then(res => {
                 if (res.ok) {
+                    setIsLoading(false)
+                    history.push('/reset-password')
                     return res.json()
                 }
+                setIsLoading(false)
                 return Promise.reject(`Ошибка ${res.status}`)
             })
-            .then(data => console.log(data))
-            .catch(error => console.log(error))
+            .catch(error => setIsLoading(false) && console.log(error))
     }
 
     return (
         <div className={forgotPasswordStyles.wrapper}>
-            <div className={forgotPasswordStyles.container}>
-                <h2 className={`${forgotPasswordStyles.heading} mb-6`}>Восстановление пароля</h2>
-                <EmailInput
-                    onChange={e => setEmail(e.target.value)}
-                    value={email}
-                    name={'email'}
-                    isIcon={false}
-                    extraClass='mb-6'
-                    placeholder="Укажите e-mail"
-                />
-                <Button 
-                    htmlType="button" 
-                    type="primary" 
-                    size="medium" 
-                    extraClass='mb-20'
-                    onClick={() => onClick(email)}
-                > {/* TODO: fix this button */}
-                    Восстановить
-                </Button>
-                <div className={`${forgotPasswordStyles.paragraph} mb-4`}>
-                    <p className="text text_type_main-default text_color_inactive">Вспомнили пароль?</p>
-                    <Link className={forgotPasswordStyles.link} to='/login'>Войти</Link>
+            { isLoading ? <LoadingSpinner /> : 
+                <div className={forgotPasswordStyles.container}>
+                    <h2 className={`${forgotPasswordStyles.heading} mb-6`}>Восстановление пароля</h2>
+                    <form className={forgotPasswordStyles.form} onSubmit={e => e.preventDefault()}>
+                        <EmailInput
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
+                            name={'email'}
+                            isIcon={false}
+                            extraClass='mb-6'
+                            placeholder="Укажите e-mail"
+                        />
+                        <Button 
+                            htmlType="submit" 
+                            type="primary" 
+                            size="medium" 
+                            extraClass='mb-20'
+                            onClick={() => onClick(email)}
+                        > 
+                            Восстановить
+                        </Button>
+                    </form>
+                    <div className={`${forgotPasswordStyles.paragraph} mb-4`}>
+                        <p className="text text_type_main-default text_color_inactive">Вспомнили пароль?</p>
+                        <Link className={forgotPasswordStyles.link} to='/login'>Войти</Link>
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
