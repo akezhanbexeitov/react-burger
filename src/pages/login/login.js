9bc7712087d19d34c4e6780445f08f7aeda98294
Akezhan <akidulatini@gmail.com>
2022-12-08 23:07:20 +0800

    Add loading spinner in login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,51 +4,55 @@ import { useState } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser } from '../../services/actions/auth'
+import LoadingSpinner from '../../components/loading-spinner/loading-spinner'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const dispatch = useDispatch()
     const user = useSelector(store => store.auth.user)
+    const isLoading = useSelector(store => store.auth.loginUserRequest)
 
     return (
         <div className={loginStyles.wrapper}>
-            <div className={loginStyles.container}>
-                <h2 className={`${loginStyles.heading} mb-6`}>Вход</h2>
-                <form className={loginStyles.form} onSubmit={e => e.preventDefault()}>
-                    <EmailInput
-                        onChange={e => setEmail(e.target.value)}
-                        value={email}
-                        name={'email'}
-                        isIcon={false}
-                        extraClass='mb-6'
-                    />
-                    <PasswordInput
-                        onChange={e => setPassword(e.target.value)}
-                        value={password}
-                        name={'password'}
-                        extraClass="mb-6"
-                    />
-                    <Button 
-                        htmlType="submit" 
-                        type="primary" 
-                        size="medium" 
-                        extraClass='mb-20'
-                        onClick={() => dispatch(loginUser(email, password))}
-                    > 
-                        Войти
-                    </Button>
-                </form>
-                <div className={`${loginStyles.paragraph} mb-4`}>
-                    <p className="text text_type_main-default text_color_inactive">Вы - новый пользователь?</p>
-                    <Link className={loginStyles.link} to='/register'>Зарегистрироваться</Link>
+            { isLoading ? <LoadingSpinner /> : 
+                <div className={loginStyles.container}>
+                    <h2 className={`${loginStyles.heading} mb-6`}>Вход</h2>
+                    <form className={loginStyles.form} onSubmit={e => e.preventDefault()}>
+                        <EmailInput
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
+                            name={'email'}
+                            isIcon={false}
+                            extraClass='mb-6'
+                        />
+                        <PasswordInput
+                            onChange={e => setPassword(e.target.value)}
+                            value={password}
+                            name={'password'}
+                            extraClass="mb-6"
+                        />
+                        <Button 
+                            htmlType="submit" 
+                            type="primary" 
+                            size="medium" 
+                            extraClass='mb-20'
+                            onClick={() => dispatch(loginUser(email, password))}
+                        > 
+                            Войти
+                        </Button>
+                    </form>
+                    <div className={`${loginStyles.paragraph} mb-4`}>
+                        <p className="text text_type_main-default text_color_inactive">Вы - новый пользователь?</p>
+                        <Link className={loginStyles.link} to='/register'>Зарегистрироваться</Link>
+                    </div>
+                    <div className={loginStyles.paragraph}>
+                        <p className="text text_type_main-default text_color_inactive">Забыли пароль?</p>
+                        <Link className={loginStyles.link} to='/forgot-password'>Восстановить пароль</Link>
+                    </div>
                 </div>
-                <div className={loginStyles.paragraph}>
-                    <p className="text text_type_main-default text_color_inactive">Забыли пароль?</p>
-                    <Link className={loginStyles.link} to='/forgot-password'>Восстановить пароль</Link>
-                </div>
-                { user ? <Redirect to='/'/> : null }
-            </div>
+            }
+            { user ? <Redirect to='/'/> : null }
         </div>
     )
 }
