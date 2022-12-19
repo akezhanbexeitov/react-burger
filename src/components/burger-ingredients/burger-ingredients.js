2becd252cc89664bfe02fddfb6787088a2ee2613
Akezhan <akidulatini@gmail.com>
2022-12-19 14:39:56 +0800

    Switch tabs when scrolling ingredients list

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,32 +1,65 @@
-import { useMemo, memo } from 'react'
+import { useMemo, memo, useRef, useState } from 'react'
 import burgerIngredientsStyles from './burger-ingredients.module.css'
-import IngredientsCategories from './ingredient-categories'
 import IngredientList from './ingredient-list'
 import { useSelector } from 'react-redux'
-import { useInView } from 'react-intersection-observer'
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+
 
 const BurgerIngredients = () => {
     const data = useSelector(store => store.ingredientsList.ingredients)
-    const { ref: bunsRef, inView: bunsInView } = useInView()
-    const { ref: saucesRef, inView: saucesInView } = useInView()
-    const { ref: mainsRef, inView: mainsInView } = useInView()
+    const [current, setCurrent] = useState('buns')
+    const tabsRef = useRef()
+    const bunsRef = useRef()
+    const saucesRef = useRef()
+    const mainsRef = useRef()
     const buns = useMemo(() => data.filter(item => item.type === 'bun' ? item : null), [data])
     const sauces = useMemo(() => data.filter(item => item.type === 'sauce' ? item : null), [data])
     const mains = useMemo(() => data.filter(item => item.type === 'main' ? item : null), [data])
+
+    const scrollView = (ingredientTypeRef) => {
+        ingredientTypeRef.current.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const handleScrollGroups = () => {
+        const tabsBottom = tabsRef.current?.getBoundingClientRect().bottom;
+        const bunsTop = bunsRef.current?.getBoundingClientRect().top;
+        const saucesTop = saucesRef.current?.getBoundingClientRect().top;
+        const mainsTop = mainsRef.current?.getBoundingClientRect().top;
+    
+        if (!tabsBottom || !bunsTop || !saucesTop || !mainsTop) {
+            return;
+        }
+    
+        const bunsDelta = Math.abs(bunsTop - tabsBottom);
+        const saucesDelta = Math.abs(saucesTop - tabsBottom);
+        const mainsDelta = Math.abs(mainsTop - tabsBottom);
+    
+        const min = Math.min(bunsDelta, saucesDelta, mainsDelta);
+    
+        const newTab =
+            min === bunsDelta ? "buns" : min === saucesDelta ? "sauces" : "mains";
+    
+        if (newTab !== current) {
+            setCurrent(newTab);
+        }
+      };
 
     return (
         <section className='mr-5'>
             <div className={burgerIngredientsStyles.container}>
                 <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-                <IngredientsCategories 
-                    bunsInView={bunsInView} 
-                    saucesInView={saucesInView} 
-                    mainsInView={mainsInView} 
-                    bunsRef={bunsRef} 
-                    saucesRef={saucesRef} 
-                    mainsRef={mainsRef}
-                />
-                <div className={burgerIngredientsStyles.ingredients}>
+                <div ref={tabsRef} className={burgerIngredientsStyles.categories}>
+                    <Tab value="buns" active={current === 'buns'} onClick={() => {setCurrent('buns'); scrollView(bunsRef)}}>
+                        Булки
+                    </Tab>
+                    <Tab value="sauces" active={current === 'sauces'} onClick={() => {setCurrent('sauces'); scrollView(saucesRef)}}>
+                        Соусы
+                    </Tab>
+                    <Tab value="mains" active={current === 'mains'} onClick={() => {setCurrent('mains'); scrollView(mainsRef)}}>
+                        Начинки
+                    </Tab>
+                </div>
+                <div onScroll={handleScrollGroups} className={burgerIngredientsStyles.ingredients}>
                     <IngredientList ref={bunsRef} ingredientType={buns} title='Булки'/>
                     <IngredientList ref={saucesRef} ingredientType={sauces} title='Соусы'/>
                     <IngredientList ref={mainsRef} ingredientType={mains} title='Начинки'/>
