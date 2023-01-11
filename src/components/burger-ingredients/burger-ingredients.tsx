6cab7577a822b44a77dbf5912008e4964f74d3d0
Akezhan <akidulatini@gmail.com>
2023-01-11 15:59:21 +0800

    Rewrite burger-ingredients to TS

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -3,24 +3,25 @@ import burgerIngredientsStyles from './burger-ingredients.module.css'
 import IngredientList from './ingredient-list'
 import { useSelector } from 'react-redux'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+import { TIngredientList } from '../../utils/types'
 
 const BurgerIngredients = () => {
-    const data = useSelector(store => store.ingredientsList.ingredients)
+    const data = useSelector((store: TIngredientList) => store.ingredientsList.ingredients)
     const [current, setCurrent] = useState('buns')
-    const tabsRef = useRef()
-    const bunsRef = useRef()
-    const saucesRef = useRef()
-    const mainsRef = useRef()
+    const tabsRef = useRef<HTMLDivElement>(null)
+    const bunsRef = useRef<HTMLHeadingElement>(null)
+    const saucesRef = useRef<HTMLHeadingElement>(null)
+    const mainsRef = useRef<HTMLHeadingElement>(null)
     const buns = useMemo(() => data.filter(item => item.type === 'bun' ? item : null), [data])
     const sauces = useMemo(() => data.filter(item => item.type === 'sauce' ? item : null), [data])
     const mains = useMemo(() => data.filter(item => item.type === 'main' ? item : null), [data])
 
-    const scrollView = (ingredientTypeRef) => {
-        ingredientTypeRef.current.scrollIntoView({ behavior: 'smooth' })
+    const scrollView = (ingredientTypeRef: HTMLElement) => {
+        ingredientTypeRef.scrollIntoView({ behavior: 'smooth' })
     }
 
     const handleScrollGroups = () => {
-        const tabsBottom = tabsRef.current?.getBoundingClientRect().bottom;
+        const tabsBottom = tabsRef.current?.getBoundingClientRect().bottom
         const bunsTop = bunsRef.current?.getBoundingClientRect().top;
         const saucesTop = saucesRef.current?.getBoundingClientRect().top;
         const mainsTop = mainsRef.current?.getBoundingClientRect().top;
@@ -48,13 +49,13 @@ const BurgerIngredients = () => {
             <div className={burgerIngredientsStyles.container}>
                 <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
                 <div ref={tabsRef} className={burgerIngredientsStyles.categories}>
-                    <Tab value="buns" active={current === 'buns'} onClick={() => {setCurrent('buns'); scrollView(bunsRef)}}>
+                    <Tab value="buns" active={current === 'buns'} onClick={() => {setCurrent('buns'); scrollView(bunsRef.current!)}}>
                         Булки
                     </Tab>
-                    <Tab value="sauces" active={current === 'sauces'} onClick={() => {setCurrent('sauces'); scrollView(saucesRef)}}>
+                    <Tab value="sauces" active={current === 'sauces'} onClick={() => {setCurrent('sauces'); scrollView(saucesRef.current!)}}>
                         Соусы
                     </Tab>
-                    <Tab value="mains" active={current === 'mains'} onClick={() => {setCurrent('mains'); scrollView(mainsRef)}}>
+                    <Tab value="mains" active={current === 'mains'} onClick={() => {setCurrent('mains'); scrollView(mainsRef.current!)}}>
                         Начинки
                     </Tab>
                 </div>
