02e707515ce59175937ba7c96a744da96b67808f
Akezhan <akidulatini@gmail.com>
2023-02-06 00:53:00 +0800

    Fix feed modal if opened as a separate tab

--- src/pages/feed-details/feed-details.tsx
+++ src/pages/feed-details/feed-details.tsx
@@ -1,10 +1,11 @@
 import feedDetailsStyles from './feed-details.module.css'
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { useSelector } from '../../utils/types'
 import { CurrencyIcon, FormattedDate } from '@ya.praktikum/react-developer-burger-ui-components'
 import { useParams } from 'react-router-dom'
 import { TOrder } from '../../services/reducers/web-socket'
 import LoadingSpinner from '../../components/loading-spinner/loading-spinner'
+import { request } from '../../utils/server-requests'
 
 type TParams = { id: string } 
 
@@ -13,17 +14,18 @@ const FeedDetails: FC = () => {
     const orders = useSelector(store => store.feed.message.orders)
     const ingredients = useSelector(store => store.ingredientsList.ingredients)
     const order = orders && orders.find((item: TOrder) => item.number === +params.id)
+    const [alternativeOrder, setAlternativeOrder] = useState<TOrder | null>(null)
     let price = 0
 
     let orderStatus
     let statusColor = '#AAA'
-    if (order && order.status === 'created') {
+    if ((order && order.status === 'created') || (alternativeOrder && alternativeOrder.status === 'created')) {
         orderStatus = 'Создан'
         statusColor = '#AAA'
-    } else if (order && order.status === 'pending') {
+    } else if ((order && order.status === 'pending') || (alternativeOrder && alternativeOrder.status === 'pending')) {
         orderStatus = 'Готовится'
         statusColor = '#FFCC66'
-    } else if (order && order.status === 'done') {
+    } else if ((order && order.status === 'done') || (alternativeOrder && alternativeOrder.status === 'done')) {
         orderStatus = 'Выполнен'
         statusColor = '#00CCCC'
     }
@@ -41,15 +43,28 @@ const FeedDetails: FC = () => {
     }
 
     const orderIngredients = order && countIdOccurrences(order.ingredients)
-    console.log(orderIngredients)
+    const alternativeOrderIngredients = alternativeOrder && countIdOccurrences(alternativeOrder.ingredients)
+
+    useEffect(() => {
+        if (!orders) {
+            const requestOptions = {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            request(`https://norma.nomoreparties.space/api/orders/${params.id}`, requestOptions)
+                .then(data => setAlternativeOrder(data.orders[0]))
+        }
+    }, [orders, params.id])
 
     return (
         <main className={feedDetailsStyles.wrapper}>
             {order
             ? <>
-                <p className={`${feedDetailsStyles.orderNumber} text text_type_digits-default mb-10`}>#{order && order.number}</p>
+                <p className={`${feedDetailsStyles.orderNumber} text text_type_digits-default mb-5`}>#{order && order.number}</p>
                 <p className="text text_type_main-medium mb-3">{order && order.name}</p>
-                <p className='text text_type_main-default mb-15' style={{ color: statusColor }}>{orderStatus}</p>
+                <p className='text text_type_main-default mb-5' style={{ color: statusColor }}>{orderStatus}</p>
                 <p className='text text_type_main-medium mb-6'>Состав:</p>
                 <div className={`${feedDetailsStyles.ingredients} pr-6 mb-10`}>
                     {order &&
@@ -108,7 +123,49 @@ const FeedDetails: FC = () => {
                     </div>
                 </div>
             </>
-            : <LoadingSpinner />}
+            : alternativeOrder 
+            ? 
+                <>
+                    <p className={`${feedDetailsStyles.orderNumber} text text_type_digits-default mb-10 mt-20`}>#{alternativeOrder && alternativeOrder.number}</p>
+                    <p className="text text_type_main-medium mb-3">{alternativeOrder && alternativeOrder.name}</p>
+                    <p className='text text_type_main-default mb-15' style={{ color: statusColor }}>{orderStatus}</p>
+                    <p className='text text_type_main-medium mb-6'>Состав:</p>
+                    <div className={`${feedDetailsStyles.ingredients} pr-6 mb-10`}>
+                        {alternativeOrder &&
+                            alternativeOrderIngredients!.map((ingredient, index) => {
+                                const currentIngredient = ingredients.find(item => item._id === ingredient.id)
+                                price += currentIngredient!.price * ingredient.count
+                                return (
+                                    <div key={index} className={`${feedDetailsStyles.ingredient} mb-4`}>
+                                        <div className={feedDetailsStyles.ingredient_left}>
+                                            <div className={`${feedDetailsStyles.image_outer} mr-4`}>
+                                                <div className={feedDetailsStyles.image_inner}>
+                                                    <img src={currentIngredient!.image} alt={currentIngredient!.name}/>
+                                                </div>
+                                            </div>
+                                            <p className={`${feedDetailsStyles.name} mr-4`}>{currentIngredient!.name}</p>
+                                        </div>
+                                        <div className={feedDetailsStyles.ingredient_right}>
+                                            <div className={feedDetailsStyles.price}>
+                                                <p className='text text_type_digits-default mr-2'>{ingredient.count === 1 ? currentIngredient!.price : `${ingredient.count} x ${currentIngredient!.price}`}</p>
+                                                <CurrencyIcon type="primary" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className={`${feedDetailsStyles.total} mb-20`}>
+                        <p className="text text_type_main-default text_color_inactive">{alternativeOrder && <FormattedDate date={new Date(alternativeOrder.createdAt)}/>}</p>
+                        <div className={feedDetailsStyles.total_price}>
+                            <p className='text text_type_digits-default mr-2'>{price}</p>
+                            <CurrencyIcon type="primary" />
+                        </div>
+                    </div>
+                </>
+            : <LoadingSpinner />
+            }
         </main>
     )
 }
